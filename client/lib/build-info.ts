67d773281b713602e916ac3302640463dfc79aa1
chenwl <chenwanglai@vip.qq.com>
2026-05-21 17:16:54 +0800

    chore: bump version to 0.5.8

--- client/lib/build-info.ts
+++ client/lib/build-info.ts
@@ -1,3 +1,3 @@
 /** 由 scripts/write-build-version.mjs 自动生成，请勿手改 */
-export const APP_VERSION = "0.5.7";
-export const BUILD_ID = "202605210907-ec8b36b";
+export const APP_VERSION = "0.5.8";
+export const BUILD_ID = "202605210916-e296939";
