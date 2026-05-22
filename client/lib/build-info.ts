162bbe6bbd997928163284eea289c7311e4aa477
chenwanglai <chenwanglai@vip.qq.com>
2026-05-23 02:19:38 +0800

    2222

--- client/lib/build-info.ts
+++ client/lib/build-info.ts
@@ -1,3 +1,3 @@
 /** 由 scripts/write-build-version.mjs 自动生成，请勿手改 */
-export const APP_VERSION = "0.6.0";
-export const BUILD_ID = "202605221814-d8568d5";
+export const APP_VERSION = "0.6.5";
+export const BUILD_ID = "202605221817-9832468";
